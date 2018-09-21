f753cf6870924bf03b2c3c1ba05cebd5849b86bd
Payam Rafiee <payamrafiee@Payams-Mac-mini-2.local>
2018-09-22 00:10:12 +0300

    add signup validations

--- src/modules/users/user.model.js
+++ src/modules/users/user.model.js
@@ -1,5 +1,8 @@
 import mongoose, { Schema } from 'mongoose';
+import validator from 'validator';
 import { hashSync, compareSync } from 'bcrypt-nodejs';
+
+import { passwordReg } from './user.validations';
 
 const UserSchema = new Schema({
   email: {
@@ -7,12 +10,24 @@ const UserSchema = new Schema({
     trim: true,
     unique: true,
     required: [true, 'Phone number is required!'],
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: '{VALUE} is not a valid email!',
+    },
   },
   password: {
     type: String,
     trim: true,
-    required: true,
-    minlength: 6,
+    required: [true, 'Password is required!'],
+    minlength: [6, 'Password need to be longer!'],
+    validate: {
+      validator(password) {
+        return passwordReg.test(password);
+      },
+      message: '{VALUE} is not a valid password!',
+    },
   },
 });
 
--- src/modules/users/user.routes.js
+++ src/modules/users/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import validate from 'express-validation';
 
 import * as userController from './user.controllers';
+import userValidation from './user.validations';
 
 const routes = new Router();
 
-routes.post('/signup', userController.signup);
+routes.post('/signup', validate(userValidation.signup), userController.signup);
 routes.post('/login', userController.login);
 
 export default routes;
